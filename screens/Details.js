e34617b3b2ff9ade59c6076bd7e5a68708954e9f
shashwat shukla <72383260+shashwatshukla123@users.noreply.github.com>
2021-11-13 14:53:39 +0530

    Update Details.js

--- screens/Details.js
+++ screens/Details.js
@@ -8,7 +8,7 @@ export default class DetailsScreen extends Component {
     this.state = {
       details: {},
       imagePath: "",
-      url: `http://localhost:5000/planet?name=${this.props.navigation.getParam(
+      url: `http://127.0.0.1:5000/planet?name=${this.props.navigation.getParam(
         "planet_name"
       )}`
     };
